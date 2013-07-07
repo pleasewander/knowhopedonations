2fca66cd13dcd9c63ba4f7bc0c623256d09c9791
pleasewander <stiltz.ryan@gmail.com>
2013-07-07 14:16:41 -0700

    edited the deploy settings

--- knowhope/settings.py
+++ knowhope/settings.py
@@ -132,6 +132,8 @@ INSTALLED_APPS = (
      'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
      'django.contrib.admindocs',
+    'storages',
+    'knowhope',
 )
 
 # A sample logging configuration. The only tangible logging
